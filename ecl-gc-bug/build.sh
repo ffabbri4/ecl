3d42c102f1fed51a8a738991a39a8f0db06c34fb
Matthew Mondor <mmondor@pulsar-zone.net>
2015-09-08 02:55:21 -0400

    Add ECL build script for debug config

---
+++ ecl-gc-bug/build.sh
@@ -0,0 +1,16 @@
+# AMD64
+export CFLAGS="-O2 -g"
+export LDFLAGS='-g'
+
+# i686
+#export CFLAGS="-march=i686 -O2 -g
+#export LDFLAGS='-g'
+
+make distclean >/tmp/ecl-clean.txt 2>&1
+
+./configure --prefix=/usr/local/gitlab-ecl --enable-unicode --enable-threads --with-__thread=no --enable-rpath --with-system-gmp --with-gmp-prefix=/usr/pkg --enable-boehm=included --with-dffi=system >/tmp/ecl-config.txt 2>&1
+
+# Note: parallel build seems broken with ECL, don't use -j for now
+nice make >/tmp/ecl-build.txt 2>&1
+
+alert
